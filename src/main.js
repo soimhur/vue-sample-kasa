c15ec09724318722480712f02eda8ca99da3e276
soimhur <soimhur@gmail.com>
2018-09-08 00:41:44 +0900

    Install semantic ui thr npm

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import SuiVue from 'semantic-ui-vue';
+
 
 Vue.config.productionTip = false
+Vue.sue(SuiVue);
 
 /* eslint-disable no-new */
 new Vue({
